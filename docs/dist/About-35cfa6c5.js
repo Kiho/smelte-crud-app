174391e20b7bd4268a05736896ea01353be218b7
Kiho <kihoc22@gmail.com>
2019-08-04 12:07:09 -0700

    Rebuild docs

--- docs/dist/About-35cfa6c5.js
+++ docs/dist/About-35cfa6c5.js
@@ -1,1 +1,2 @@
 import{S as s,i as t,s as a,e,b as n,B as o,j as r}from"./chunk-dd27efbb.js";function u(s){var t;return{c(){(t=e("h1")).textContent="About"},m(s,a){n(s,t,a)},p:o,i:o,o:o,d(s){s&&r(t)}}}export default class extends s{constructor(s){super(),t(this,s,null,u,a,[])}}
+//# sourceMappingURL=About-35cfa6c5.js.map
