20721ab343328bfa129e704fa2629da24fc6a700
Kiho <kihoc22@gmail.com>
2019-08-08 22:10:59 -0700

    Update docs

---
+++ docs/dist/About-96aa54cf.js
@@ -0,0 +1,1 @@
+import{S as s,i as t,s as e,e as a,b as n,F as o,j as r}from"./chunk-2012ee68.js";function u(s){var t;return{c(){(t=a("h1")).textContent="About"},m(s,e){n(s,t,e)},p:o,i:o,o:o,d(s){s&&r(t)}}}export default class extends s{constructor(s){super(),t(this,s,null,u,e,[])}}
